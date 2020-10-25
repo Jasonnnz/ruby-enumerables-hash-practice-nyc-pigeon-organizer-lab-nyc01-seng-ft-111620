6ade2216709ba8b27f340aa3f5cce763fc37daf4
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-25 23:02:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,5 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  # data -> {:color=>{},:gender=>{},:lives=>{}} k - color,gender,lives
-  # v - hashes {:purple,:grey,:white,:brown},{:male,:female},{subway, central park, lbrary,city hall}
-  
   pigeon_list = {}
   
 end
